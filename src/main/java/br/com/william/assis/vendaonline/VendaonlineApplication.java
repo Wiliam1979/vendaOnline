e9eb490e9c10131b9c422d45acf70f798425ec8b
William Assis <willifran@gmail.com>
2020-11-04 12:42:44 -0300

    criando o ambiente de desenvolvimento e produção e as instanciações da entidade categoria

--- src/main/java/br/com/william/assis/vendaonline/VendaonlineApplication.java
+++ src/main/java/br/com/william/assis/vendaonline/VendaonlineApplication.java
@@ -1,13 +1,31 @@
 package br.com.william.assis.vendaonline;
 
+import br.com.william.assis.vendaonline.domain.Categoria;
+import br.com.william.assis.vendaonline.repositores.CategoriaRepositore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VendaonlineApplication {
+public class VendaonlineApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepositore categoriaRepositore;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendaonlineApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório" );
+
+		categoriaRepositore.saveAll(Arrays.asList(cat1, cat2));
+
+	}
 }
