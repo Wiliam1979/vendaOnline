142e9fc54c6266354897017554f6d2a368a03aeb
William F Assis <willifran@gmail.com>
2021-03-03 17:40:22 -0300

    Criando o profile  de teste

---
+++ src/main/java/br/com/william/assis/vendaonline/config/TesteConfig.java
@@ -0,0 +1,24 @@
+package br.com.william.assis.vendaonline.config;
+
+import br.com.william.assis.vendaonline.service.DBservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("development")
+public class TesteConfig {
+
+    @Autowired
+    private DBservice dBservice;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+
+        dBservice.instantiateTestDataBase();
+        return true;
+    }
+}
